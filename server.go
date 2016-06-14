e20b4e6ef4ca7526d0614dad3661fecf2df7afa8
ElyKar <tristan.claverie@laposte.net>
2016-06-14 20:23:37 +0200

    Minimal example

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+type Operation struct {
+	Type     string  `json:"type"`
+	OperandA float64 `json:"opA"`
+	OperandB float64 `json:"opB"`
+}
+
+var operations [4]string
+
+func NewRandomOp() *Operation {
+	a := rand.Float64() + 1
+	b := rand.Float64() + 1
+	op := operations[rand.Int()%4]
+	return &Operation{
+		Type:     op,
+		OperandA: a,
+		OperandB: b,
+	}
+}
+
+func (o *Operation) AddOne() {
+	o.OperandA += 1
+}
+
+func init() {
+	operations = [4]string{"ADD", "SUB", "MUL", "DIV"}
+}
+
+func handler(resp http.ResponseWriter, req *http.Request) {
+	operation := NewRandomOp()
+	encoded, err := json.Marshal(operation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resp.Write([]byte(req.RemoteAddr + "\n"))
+	resp.Write(encoded)
+}
+
+func main() {
+	http.HandleFunc("/question", handler)
+	http.ListenAndServe(":8000", nil)
+}
